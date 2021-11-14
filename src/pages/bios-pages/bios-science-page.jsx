21138df656de98c34644bea94b0ce9b139603893
weirjulieanne <85594164+weirjulieanne@users.noreply.github.com>
2021-11-14 08:07:59 -0700

    changed bio-cards removed colour border so they look more clean and modern

---
+++ src/pages/bios-pages/bios-science-page.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { ScienceBioCardComponent } from "../../components/bio-cards/science-bio-card.component";
