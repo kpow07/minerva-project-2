3cc8584ab6e1dced44b46c29c23950187128dcaa
weirjulieanne <85594164+weirjulieanne@users.noreply.github.com>
2021-11-13 21:41:36 -0700

    added a science card component prototype  with styling.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
 import "./App.css";
+import BioCardComponent from "./components/bio-card/bio-card.component";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BioCardComponent />
     </div>
   );
 }
--- src/components/bio-card/bio-card.component.jsx
+++ src/components/bio-card/bio-card.component.jsx
@@ -1,8 +1,39 @@
-import React from "react";
-import "./bio-card.component.css";
+import React, { useState } from "react";
+import "./bio-rem.css";
 
 function BioCardComponent() {
-  return <div></div>;
+  const [name, setName] = useState("MARIE CURIE");
+  const [job, setJob] = useState("SCIENTIST");
+  const [description, setDescription] = useState(
+    "This is a brief description about so and so "
+  );
+
+  return (
+    <div className="bio-card">
+      <div className="upper-container">
+        <img
+          className="portrait"
+          src="stempics/Marie-Curie-1-CROPPED.jpg"
+          alt="marie curie"
+          height="230px"
+          styles={{ backgroundPosition: "center" }}
+        />
+        <div className="image-container">
+          <img
+            src="images/logos/beaker.png"
+            alt="logo"
+            width="25px"
+            height="25px"
+          />
+        </div>
+      </div>
+      <div className="lower-container">
+        <h3>{name}</h3>
+        <h4>{job}</h4>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default BioCardComponent;
