933b78be8e507987d72056976e4a5eda85fe2097
kpow07 <kpow@shaw.ca>
2021-11-20 12:31:26 -0700

    adding Footer.js/Footer.css/image

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import MasterForm from "./components/forms/MasterForm";
+import Footer from "./components/nav-bar/Footer";
 
 function App() {
   return (
     <div className="App">
       <MasterForm />
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/nav-bar/Footer.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./Footer.css";
+import Social from "./hangingsocial.png";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footerAddress">
+        <h1 className="footerLogo"> MINERVA</h1>
+
+        <h2>
+          that's so fetch(💛) <br /> Michelle, Julie, Kate and Rita
+        </h2>
+
+        <address>
+          404 InceptionU Lane, Calgary, AB H3H 3H3
+          <br />
+          <a className="footerButton" href="mailto:mentoring@minerva.com">
+            Email Us
+          </a>
+        </address>
+      </div>
+      <ul className="footerNav">
+        <li className="navItem">
+          <h2 className="navTitle">Mentoring</h2>
+
+          <ul className="navList">
+            <li>Become a Mentee</li>
+
+            <li>Become a Mentor</li>
+
+            <li>Tell a Friend</li>
+          </ul>
+        </li>
+        <li className="navItem">
+          <h2 className="navTitle">Legal</h2>
+
+          <ul className="navList">
+            <li>Privacy Policy</li>
+
+            <li>Terms of Use</li>
+
+            <li>Sitemap</li>
+          </ul>
+        </li>
+        <li className="navItem">
+          <h2 className="navTitle">Connect With Us</h2>
+          <ul className="navList">
+            <li className="photo">
+              <img src={Social} alt="social media" />
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <div className="bottom">
+        <p>&copy; 2021 Project Two. All rights reserved.</p>
+
+        <div className="bottomMessage">
+          <span>
+            when women support each other, incredible things happen{" "}
+            <span className="heart">♥</span>
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
