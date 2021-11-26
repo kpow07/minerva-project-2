47abab82e67f1df88d7f51ca1e302ecca18c173e
Michlovin <michb5@hotmail.com>
2021-11-26 10:14:06 -0700

    yup

--- client/src/components/pages/LandingPageBody.js
+++ client/src/components/pages/LandingPageBody.js
@@ -3,12 +3,23 @@ import MathBioCardComponent from "../bio-cards/MathBioCardComponent";
 import EngineeringBioCardComponent from "../bio-cards/EngineeringBioCardComponent";
 import ScienceBioCardComponent from "../bio-cards/ScienceBioCardComponent";
 import TechBioCardComponent from "../bio-cards/TechBioCardComponent";
+//about us component 
 
 
-const FirstPageBody= ()=> {
+const FirstPageBody= (props)=> {
     return (
 
 <div id= "body">
+    <About 
+    className= 'section'
+    img= '.forms/mint.jpeg'
+    title = 'About Us'
+    description= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+    
+    
+    
+    
+    >
     <MathBioCardComponent/>
     <ScienceBioCardComponent/>
     <TechBioCardComponent/>
