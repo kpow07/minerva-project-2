c484e2d5480bd7a60c9d579b218d247ff65549eb
Michlovin <michb5@hotmail.com>
2021-11-26 08:46:37 -0700

    test

---
+++ client/src/components/pages/LandingPage.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import MathBioCardComponent from "../bio-cards/MathBioCardComponent";
+import EngineeringBioCardComponent from "../bio-cards/EngineeringBioCardComponent";
+import ScienceBioCardComponent from "../bio-cards/ScienceBioCardComponent";
+import TechBioCardComponent from "../bio-cards/TechBioCardComponent";
+
+
+
+//style card in grid 
