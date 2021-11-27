a545e9a75bf32bca9f5ac139a800fd2d2378704a
Rita Ogedegbe <ritaogedegbe@yahoo.co.uk>
2021-11-27 15:11:47 -0700

    moves App.js file in server

---
+++ server/routes/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+const router = express.Router();
+
+//@description: Login/Landing Page
+//@routes GET/
+router.get("/", (req, res) => {
+  //res.sendFile(__dirname + "/login.html");
+  res.send("Login page");
+  // redirect("/mainPage");
+});
+
+//@description: Main Page
+//@routes GET/
+router.get("/mainPage", (req, res) => {
+  res.send("Main Page");
+});
+
+export default router;
