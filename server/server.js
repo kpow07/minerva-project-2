ae76e7bc71b76d46881e827c383a32fd8c772d91
kpow07 <kpow@shaw.ca>
2021-11-29 15:57:56 -0700

    updating port to 5000

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ import apiRouter from "./routes/apiRouter.js";
 import multer from "multer";
 
 const app = express();
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 const MongoStore = connectMongo(session);
 
 //Load config
@@ -62,7 +62,7 @@ app.use(passport.session());
 
 //Routes
 app.use("/api", apiRouter);
-//description: http://localhost:6000/api/auth
+//description: http://localhost:5000/api/auth
 app.use("/auth", authRouter);
 
 //Server
