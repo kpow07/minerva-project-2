4b896c75cadd9a1af68b16cc250800a4918a93a7
Michlovin <michb5@hotmail.com>
2021-12-01 01:16:15 -0700

    playing with css for box

--- client/src/components/pages/AboutComponent.js
+++ client/src/components/pages/AboutComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Landingstyles from "./Landingstyles.css"
+import branch from "./branch.png"
 
 
 function AboutComponent() {
@@ -11,7 +12,17 @@ function AboutComponent() {
 
         
       <div className="about-container">
-        <h1>About</h1>
+        <h1>About Us: Minerva Mentoring</h1>
+        <p> 
+          <img src={branch} alt="olive branch" />
+        </p>
+         <div className="grid-item">
+           
+        <h4> When women support each other, incredible things happen</h4>
+        <p>Strong ut chicory crema to go cinnamon cup percolator con panna. Cappuccino, single shot crema, beans con panna barista café au lait whipped cup frappuccino trifecta carajillo. Espresso, beans doppio french press ristretto pumpkin spice aromatic. Café au lait, dark, et dripper crema instant cinnamon java.</p>
+
+        
+        </div>
       </div>
       
     );
