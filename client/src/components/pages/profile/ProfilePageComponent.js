d500e0e82ac9e0b7223dd8c1b07cb3a1fab9dd58
Michlovin <michb5@hotmail.com>
2021-12-07 15:31:35 -0700

    styles for profile page

--- client/src/components/pages/profile/ProfilePageComponent.js
+++ client/src/components/pages/profile/ProfilePageComponent.js
@@ -2,19 +2,33 @@ import React from "react";
 import { useNavigate, useParams } from "react-router";
 import AboutMe from "./AboutMe";
 import ProfilePageCardDiv from "./ProfilePageCardDiv";
+import "./ProfilePage.css"
 
 // this will render the Mentor Card, About Me & Q&A and like button
 
 function ProfilePageComponent() {
+ 
   let navigate = useNavigate();
   let params = useParams();
 
+
   return (
-    <div>
-      <div className="page-container">
+    
+<div className="wrapper">
+<div id="card-div-title" className="header">
+          <h1>Mentor Profile Page</h1>
+        </div> 
+     
+      <div className="sidebar">
         <ProfilePageCardDiv mentorId={params.id} />
       </div>
+      {/* <div className="content">
+        <AboutMe mentorId={mentorId} />
+      </div> */}
     </div>
+  
+  
+    
   );
 }
 export default ProfilePageComponent;
