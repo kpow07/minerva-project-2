82aec72f8f2b2acbf062e21741de17d166c7a8ce
Michlovin <michb5@hotmail.com>
2021-12-07 19:04:03 -0700

    MessageBoard

--- client/src/components/pages/profile/MessageBoard.js
+++ client/src/components/pages/profile/MessageBoard.js
@@ -1,10 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
+// import CommentForm from "./CommentForm"
+// import CommentList from "./CommentList"
+// import CommentBox from "./CommentBox"
+// import "./ProfilePage.css";
 
-class Welcome extends Component {
-   state = {};
-   render() {
-      return<div>Hello {this.props.name}</div>;
-   }
-}
+const MessageBoard = () => {
+  state = {};
+  render();
+  return <div>Hello {this.props.name}</div>;
 
-export default Welcome;
+  <div className="wrapper">
+    <div id="card-div-title" className="header">
+      <h1>Message Board</h1>
+    </div>
+
+    <div className="sidebar">
+      <CommentForm />
+    </div>
+    <div className="content">
+      <CommentList />
+
+      <div>
+        <CommentBox />
+      </div>
+    </div>
+  </div>;
+};
+
+export default MessageBoard;
--- client/src/components/pages/profile/ProfilePageComponent.js
+++ client/src/components/pages/profile/ProfilePageComponent.js
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import AboutMe from "./AboutMe";
 import ProfilePageCardDiv from "./ProfilePageCardDiv";
-import "./ProfilePage.css"
+import "./ProfilePage.css";
 
 // this will render the Mentor Card, About Me & Q&A and like button
 
-function ProfilePageComponent({mentorId}) {
+function ProfilePageComponent({ mentorId }) {
   const [mentor, setMentor] = useState("");
   let navigate = useNavigate();
   let params = useParams();
@@ -22,14 +22,12 @@ function ProfilePageComponent({mentorId}) {
     fetchMentor();
   }, [mentorId]);
 
-
   return (
-    
-<div className="wrapper">
-<div id="card-div-title" className="header">
-          <h1>Mentor Profile Page</h1>
-        </div> 
-     
+    <div className="wrapper">
+      <div id="card-div-title" className="header">
+        <h1>Mentor Profile Page</h1>
+      </div>
+
       <div className="sidebar">
         <ProfilePageCardDiv mentorId={params.id} />
       </div>
@@ -37,13 +35,11 @@ function ProfilePageComponent({mentorId}) {
         <AboutMe mentorId={params.id} />
       </div>
 
-
-     {/* <div>
+      {/* <div>
        <MessageBoard className="footer"/>
      </div> */}
-   </div>
-    
+    </div>
   );
-};
+}
 export default ProfilePageComponent;
 //fetch to take ID from URL , if the user wants to comment they need to pull it out of the URL and it pulls the name out of it
