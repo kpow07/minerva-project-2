6a9439224aa78ca9f2faf6a123033872478147fb
Kate Power <90926721+kpow07@users.noreply.github.com>
2021-12-16 08:56:41 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ import BioEditPage from "./components/pages/bio edit page/BioEditPage";
 import MentorPageComponent from "./components/pages/mentor page/MentorPageComponent";
 import ProfilePageComponent from "./components/pages/profile/ProfilePageComponent";
 import MentorEditPage from "./components/pages/mentor-edit-page/MentorEditPage";
-import MenteeFavPage from "./components/pages/menteefavpage/menteeFavPage";
+import MenteeFavPage from "./components/pages/menteefavpage/MenteeFavPage";
 //import EditMentorForm from "./components/forms/forms/EditMentorForm";
 //import { useState } from "react";
 
